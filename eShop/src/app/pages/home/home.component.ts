dfe42eced4d752244ba4a892fccd4d8ed0a60e1f
Nikifor Dragov <ndragov99@gmail.com>
2023-07-14 15:42:57 +0300

    Get products from api

--- eShop/src/app/pages/home/home.component.ts
+++ eShop/src/app/pages/home/home.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  // change all any-s according to types
+  productList: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadAllProducts();
+  }
+
+  loadAllProducts() {
+    this.productService.getAllProducts().subscribe((result: any) => {
+      console.log(result.data);
+
+      this.productList = result.data;
+    });
+  }
 }
