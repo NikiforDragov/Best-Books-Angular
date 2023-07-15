f702a908dbace49601c72792eb038bfafbe238f9
Nikifor Dragov <ndragov99@gmail.com>
2023-07-15 18:33:31 +0300

    Add checkout func

--- eShop/src/app/app.component.ts
+++ eShop/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductService } from './services/product.service';
 
 @Component({
@@ -6,22 +7,32 @@ import { ProductService } from './services/product.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'eShop';
 
   cartProducts: any[] = [];
+  subTotal: number = 0;
 
-  constructor(private productService: ProductService) {
-    this.productService.cartAddedSubject.subscribe((res) => {});
+  constructor(private productService: ProductService, private router: Router) {
+    this.productService.cartAddedSubject.subscribe((res) => {
+      this.loadCart();
+    });
   }
 
   ngOnInit(): void {
     this.loadCart();
   }
 
+  redirectToSale() {
+    this.router.navigateByUrl('sale');
+  }
+
   loadCart() {
     this.productService.getCartItemsByCustomerId(1).subscribe((res: any) => {
       this.cartProducts = res.data;
+      this.cartProducts.forEach((element) => {
+        this.subTotal = element.productPrice;
+      });
     });
   }
 }
--- eShop/src/app/pages/home/home.component.ts
+++ eShop/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,11 +11,11 @@ export class HomeComponent implements OnInit {
   // change all any-s according to types
   productList: any[] = [];
   cartObj: any = {
-    CartId: 0,
-    CustId: 1,
-    ProductId: 0,
-    Quantity: 0,
-    AddedDate: '2023-07-14T12:58:09.496Z',
+    'CartId': 0,
+    'CustId': 1,
+    'ProductId': 0,
+    'Quantity': 0,
+    'AddedDate': '2023-07-14T12:58:09.496Z',
   };
 
   constructor(private productService: ProductService) {}
@@ -25,15 +26,16 @@ export class HomeComponent implements OnInit {
 
   loadAllProducts() {
     this.productService.getAllProducts().subscribe((result: any) => {
+      console.log(result.data);
+      
       this.productList = result.data;
     });
   }
 
   addItemToCart(productId: number) {
-    this.cartObj.ProductId = productId;
-
+    this.cartObj.productId = productId;
     this.productService.addToCart(this.cartObj).subscribe((result: any) => {
-      if(result.result) {
+      if(result.result) {        
         alert('Product added to cart!')
         this.productService.cartAddedSubject.next(true);
       }
